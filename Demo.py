044ed20d7f3979485fd0553f2399b8365500276d
Patrick Link <ma0263@technikum-wien.at>
2023-04-14 16:16:07 +0200

    - colorful lines

--- Demo.py
+++ Demo.py
@@ -26,7 +26,7 @@ from pyLiveLinkFace import PyLiveLinkFace, FaceBlendShape
 mp_face_mesh = mp.solutions.face_mesh
 mp_face_mesh_connections = mp.solutions.face_mesh_connections
 
-
+colors = [(166,206,227),(31,120,180),(178,223,138),(51,160,44),(251,154,153),(227,26,28),(253,191,111),(255,127,0),(202,178,214),(106,61,154),(255,255,153),(177,89,40)]
 class Demo(QThread):
     def __init__(self):
         super().__init__()
@@ -136,6 +136,8 @@ class Demo(QThread):
                 self.mouse.process_signal(self.signals)
                 # Debug
             self.annotated_landmarks = DrawingDebug.draw_landmarks_fast(np_landmarks, image)
+            for i, indices in enumerate(self.signal_calculator.ear_indices):
+                self.annotated_landmarks = DrawingDebug.draw_landmarks_fast(np_landmarks, self.annotated_landmarks, index=indices, color=colors[i%len(colors)])
 
             self.fps = self.fps_counter()
 
